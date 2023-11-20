01489f780f84fac95f4b61a1561c458c8e22f2a8
DevelopersWeb002 <129720181+DevelopersWeb002@users.noreply.github.com>
2023-11-20 10:48:20 +0530

    latest changes

--- app/Http/Controllers/OutwardController.php
+++ app/Http/Controllers/OutwardController.php
@@ -112,7 +112,6 @@ class OutwardController extends Controller
             return $array;
         }
     }
-
     public function AddOutward(Request $request)
     {
 
@@ -257,10 +256,12 @@ class OutwardController extends Controller
 
             if ($existingRecord) {
                 $getresult = DB::select("SELECT SalesNoPacks FROM `artstockstatus` WHERE outletId = '" . $data["PartyId"] . "' AND ArticleId = " . $articleId);
-                $GetNoPacks = $getresult[0]->GetNoPacks;
+                if (!empty($getresult) && isset($getresult[0]->SalesNoPacks)) {
+                    $GetNoPacks = $getresult[0]->SalesNoPacks;
                 $dataupdate = $GetNoPacks + $data['NoPacksNew'];
                 DB::table('artstockstatus')->where(['outletId' => $data['PartyId']])->where(['ArticleId' => $articleId])->update(['SalesNoPacks' => $dataupdate, 'TotalPieces' => $dataupdate]);
-            } else {
+           }
+         } else {
                 
                 $dataupdate = $data['NoPacksNew'];
                 // DB::table('artstockstatus')->where(['outletId' => $data['PartyId']])->where(['ArticleId' => $articleId])->update(['SalesNoPacks' => $dataupdate, 'TotalPieces' => $dataupdate]);
@@ -309,37 +310,37 @@ class OutwardController extends Controller
         //     }
         // }
 
-        $totalPieces = array_sum($salesNoPacksData);
-        $salesNoPacksDataString = implode(',', $salesNoPacksData);
+        // $totalPieces = array_sum($salesNoPacksData);
+        // $salesNoPacksDataString = implode(',', $salesNoPacksData);
 
-        $existingRecord = DB::table('artstockstatus')
-            ->where(['outletId' => $data['PartyId'], 'ArticleId' => $articleId])
-            ->first();
+        // $existingRecord = DB::table('artstockstatus')
+        //     ->where(['outletId' => $data['PartyId'], 'ArticleId' => $articleId])
+        //     ->first();
 
-        if ($existingRecord) {
-            // Update existing record
-            DB::table('artstockstatus')
-                ->where(['outletId' => $data['PartyId'], 'ArticleId' => $articleId])
-                ->update(['SalesNoPacks' => $salesNoPacksDataString, 'TotalPieces' => $totalPieces]);
-        } else {
-            // Insert new record
-            $isOutlet = DB::select("SELECT OutletAssign FROM `party` where Id ='" . $data['PartyId'] . "'");
-            if ($isOutlet[0]->OutletAssign == 1) { 
-            DB::table('artstockstatus')->insert([
-                'outletId' => $data['PartyId'],
-                'ArticleId' => $articleId,
-                'ArticleNumber' => $articleNumber,
-                'SalesNoPacks' => $salesNoPacksDataString,
-                'TotalPieces' => $totalPieces,
-                'ArticleColor' => $data['ArticleSelectedColor'][0]['Name'],
-                'ArticleSize' => implode(',', array_column($data['ArticleSelectedSize'], 'Name')),
-                'ArticleRatio' => $data['ArticleRatio'],
-                'ArticleOpenFlag' => $data['ArticleOpenFlag'],
-                'Title' => $data['Category'],
-                'Subcategory' => $name,
-            ]);
-            }
-        }
+        // if ($existingRecord) {
+        //     // Update existing record
+        //     DB::table('artstockstatus')
+        //         ->where(['outletId' => $data['PartyId'], 'ArticleId' => $articleId])
+        //         ->update(['SalesNoPacks' => $salesNoPacksDataString, 'TotalPieces' => $totalPieces]);
+        // } else {
+        //     // Insert new record
+        //     $isOutlet = DB::select("SELECT OutletAssign FROM `party` where Id ='" . $data['PartyId'] . "'");
+        //     if ($isOutlet[0]->OutletAssign == 1) { 
+        //     DB::table('artstockstatus')->insert([
+        //         'outletId' => $data['PartyId'],
+        //         'ArticleId' => $articleId,
+        //         'ArticleNumber' => $articleNumber,
+        //         'SalesNoPacks' => $salesNoPacksDataString,
+        //         'TotalPieces' => $totalPieces,
+        //         'ArticleColor' => $data['ArticleSelectedColor'][0]['Name'],
+        //         'ArticleSize' => implode(',', array_column($data['ArticleSelectedSize'], 'Name')),
+        //         'ArticleRatio' => $data['ArticleRatio'],
+        //         'ArticleOpenFlag' => $data['ArticleOpenFlag'],
+        //         'Title' => $data['Category'],
+        //         'Subcategory' => $name,
+        //     ]);
+        //     }
+        // }
 
 
         //close
@@ -350,7 +351,7 @@ class OutwardController extends Controller
             if ($partyrecord->UserId != null) {
                 DB::beginTransaction();
                 try {
-                    $dataresult = DB::select('SELECT c.Colorflag FROM `article` a inner join category c on c.Id=a.CategoryId where a.Id="' . $data['ArticleId'] . '"');
+                    $dataresult = DB::select('SELECT c.Colorflag FROM `article` a LEFT join category c on c.Id=a.CategoryId where a.Id="' . $data['ArticleId'] . '"');
                     $Colorflag = $dataresult[0]->Colorflag;
                     $datanopacks = DB::select('SELECT OutwardNoPacks FROM `so` where ArticleId="' . $data['ArticleId'] . '" and SoNumberId="' . $data['SoId'] . '"');
                     $search = $datanopacks[0]->OutwardNoPacks;
@@ -599,12 +600,14 @@ class OutwardController extends Controller
             return response()->json(['errorparty' => 1], 200);
         }
     }
+    
 
-
+    
     public function OutwardListFromOWNO($Id, Request $request)
     {
 
         $data = $request->all();
+        //return $data;
         $search = $data["search"];
         $startnumber = $data["start"];
         $vnddataTotal = DB::select('select count(*) as Total from (SELECT o.Id , o.PartyDiscount, o.OutwardNumberId, o.OutwardBox, o.OutwardRate,o.OutwardWeight, owdn.OutwardNumber, owdn.OutwardDate, owdn.GSTAmount, owdn.GSTPercentage, owdn.GSTType, o.NoPacks, a.ArticleNumber, a.Id as ArticleId,c.Title as Category,   concat(owdn.OutwardNumber, \'/\',fn.StartYear,\'-\',fn.EndYear) as OW_Number_FinancialYear FROM `outward` o inner join outwardnumber owdn on o.OutwardNumberId=owdn.Id inner join article a on a.Id=o.ArticleId inner join category c on c.Id=a.CategoryId inner join financialyear fn on fn.Id=owdn.FinancialYearId where o.OutwardNumberId="' . $Id . '") as d');
